cfaba48a2ba4d769ff7c6e273d331ae099b8c863
KDavee <38099279+KDavee@users.noreply.github.com>
2018-11-25 21:10:24 +0100

    Add files via upload

---
+++ bead/includes/config.inc.php
@@ -0,0 +1,31 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Mini honlap Kft.',
+);
+
+$fejlec = array(
+    'kepforras' => 'csipero-logo.png',
+    'kepalt' => 'logo',
+    'kepforras2' => 'felsokep.jpg',
+    'kepalt2' => 'logo',
+	'cim' => 'Mini honlap',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Mini honlap Kft.'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap', 'menu' => 1),
+	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menu' => 1),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menu' => 1),
+	'cikkek' => array('fajl' => 'cikkek', 'szoveg' => 'Cikkek', 'menu' => 1),
+    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menu' => 1),
+    'regisztracio' => array('fajl' => 'urlap', 'szoveg' => 'Regisztráció', 'menu' => 1),
+    'feldolgoz' => array('fajl' => 'feldolgoz', 'szoveg' => '', 'menu' => 0)
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!', 'menu' => 0);
+?>
