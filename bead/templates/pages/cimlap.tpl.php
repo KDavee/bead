d78b3d6f1da210a6c6699c27305cbb06b9b250db
KDavee <38099279+KDavee@users.noreply.github.com>
2018-11-26 11:53:44 +0100

    Add files via upload

--- bead/templates/pages/cimlap.tpl.php
+++ bead/templates/pages/cimlap.tpl.php
@@ -1,11 +1,52 @@
-<h2>Köszöntés</h2>
-<img src="./images/arc.jpg">
-<h3>Mi a Lorem Ipsum?</h3>
-<p>A Lorem Ipsum egy egyszerû szövegrészlete, szövegutánzata a betûszedõ és nyomdaiparnak. A Lorem Ipsum az 1500-as évek óta standard szövegrészletként szolgált az iparban; mikor egy ismeretlen nyomdász összeállította a betûkészletét és egy példa-könyvet vagy szöveget nyomott papírra, ezt használta. Nem csak 5 évszázadot élt túl, de az elektronikus betûkészleteknél is változatlanul megmaradt. Az 1960-as években népszerûsítették a Lorem Ipsum részleteket magukbafoglaló Letraset lapokkal, és legutóbb softwarekkel mint például az Aldus Pagemaker.</p>
-<h3>Honnan származik?</h3>
-<p>A hiedelemmel ellentétben a Lorem Ipsum nem véletlenszerû szöveg. Gyökerei egy Kr. E. 45-ös latin irodalmi klasszikushoz nyúlnak. Richarrd McClintock a virginiai Hampden-Sydney egyetem professzora kikereste az ismeretlenebb latin szavak közül az egyiket (consectetur) egy Lorem Ipsum részletbõl, és a klasszikus irodalmat átkutatva vitathatatlan forrást talált. A Lorem Ipsum az 1.10.32 és 1.10.33-as de Finibus Bonoruem et Malorum részleteibõl származik (A Jó és Rossz határai - Cicero), Kr. E. 45-bõl. A könyv az etika elméletét tanulmányozza, ami nagyon népszerû volt a reneszánsz korban. A Lorem Ipsum elsõ sora, Lorem ipsum dolor sit amet.. a 1.10.32-es bekezdésbõl származik.</p>
-<p>A Lorem Ipsum alaprészlete, amit az 1500-as évek óta használtak, az érdeklõdõk kedvéért lent újra megtekinthetõ. Az 1.10.32 és 1.10.33-as bekezdéseket szintén eredeti formájukban reprodukálták a hozzá tartozó angol változattal az 1914-es fordításból H. Rackhamtól.</p>
-<h3>Miért használjuk?</h3>
-<p>Ez egy régóta elfogadott tény, miszerint egy olvasót zavarja az olvasható szöveg miközben a szöveg elrendezését nézi. A Lorem Ipsum használatának lényege, hogy többé-kevésbé rendezettebb betûket tartalmaz, ellentétben a Tartalom helye, Tartalom helye-féle megoldással. Sok desktop szerkesztõ és weboldal szerkesztõ használja a Lorem Ipsum-ot mint alapbeállítású szövegmodellt, és egy keresés a lorem ipsum-ra sok félkész weboldalt fog eredményezni.</p>
-<h3>Honnan szerezhető be?</h3>
-<p>A Lorem Ipsum részleteinek sok változata elérhetõ, de a legtöbbet megváltoztatták egy kis humorral és véletlenszerûen kiválasztott szavakkal, amik kicsit sem teszik értelmessé. Ha használni készülsz a Lorem Ipsumot, biztosnak kell lenned abban, hogy semmi kínos sincs elrejtve a szöveg közepén. Az összes internetes Lorem Ipsum készítõ igyekszik elõre beállított részleteket ismételni a szükséges mennyiségben, ezzel téve az internet egyetlen igazi Lorem Ipsum generátorává ezt az oldalt. Az oldal körülbelül 200 latin szót használ, egy maroknyi modell-mondatszerkezettel így téve a Lorem Ipsumot elfogadhatóvá. Továbbá az elkészült Lorem Ipsum humortól, ismétlõdéstõl vagy értelmetlen szavaktól mentes.</p>
+<h1>Köszöntés</h1>
+
+<img class="nagy2" src="./images/csip1.jpg">
+<p>Az <strong>Európa Jövője Egyesület</strong> 1990. szeptember 29-én azzal a céllal alakult, hogy gyermektalálkozót szervezzen Kecskeméten, annak érdekében, hogy a kecskeméti gyerekeket megismertesse Európa gyermekeivel. Az alapötlet egyszerű volt, a kontinens országaiból gyermekeket hívnak meg, akiket kecskeméti családok látnak vendégül. A következő évben a külföldi csoportok hívják viszontlátogatásra vendéglátóikat. </p>
+<p>A gyermekek és az ifjúság nemzetközi kulturális cseréjén keresztül a magyar kultúra és Kecskemét város népszerűsítését egyik fő feladatunknak tekintjük.</p>
+<p>Programjainkon keresztül segítjük a határon túli magyarság helyzetének jobb megismerését, kulturális kapcsolatok kiépítését.</p>
+<p>Az egyesületünk emellett aktív szereplője a városi, regionális és országos civil életnek. Bács-Kiskun megyei Önkéntes Központként működött éveken keresztül, önkénteseink mellett az iskolai közösségi szolgálat során is számos diákot foglalkoztatunk programjainkon. </p>
+<p>Kiadványaikon, rendezvényeinken keresztül teszünk azért, hogy a megalakuláskor célul kitűzött eszméket szolgáljuk, így a jövő Európa polgárait, a ma gyermekeit segítsük abban, hogy előítéletektől mentes, a másságot elfogadó, toleráns nemzedékké váljanak.</p>
+
+
+<h1>Elnökség</h1>
+
+<img class="nagy" src="./images/csip2.jpg">
+<h3>Európa Jövője Egyesület elnöksége 2017-2021</h3>
+<p>2017. augusztus 30-án az egyesület közgyűlése által megválasztott elnökség:</p>
+
+<h4>Elnök:</h4>
+<ul>
+    <li>Berei Andrea</li>
+</ul>
+
+<h4>Társelnök:</h4>
+<ul>
+    <li>Dr. Kriston-Vízi József (alapító)</li>
+</ul>
+
+<h4>Elnökségi tagok:</h4>
+<ul>
+    <li>Gyurgyik Henriett</li>
+    <li>Dr. Iványosi Szabó András (alapító)</li>
+    <li>Dr. Kozmácsné Dr. Fogarasi Zsuzsanna</li>
+    <li>Mészár Zsuzsa (alapító)</li>
+    <li>Péczeli József</li>
+</ul>
+
+<h3>Az Egyeztető és Ellenőrző Bizottság: 2017-2021</h3>
+
+<h4>Elnök:</h4>
+<ul>
+    <li>Szablics Bálint</li>
+</ul>
+
+<h4>Tagok:</h4>
+<ul>
+    <li>Gera Anna</li>
+    <li>Kökény Ferencné</li>
+</ul>
+
+
+
+
+
