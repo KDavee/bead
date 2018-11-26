074bfda9acb83037319e020abb2555df764fad36
KDavee <38099279+KDavee@users.noreply.github.com>
2018-11-26 20:38:42 +0100

    Add files via upload

---
+++ bead/Adabazis_feltoltes.sql
@@ -0,0 +1,48 @@
+DROP TABLE 2018;
+
+CREATE TABLE 2018(
+ orszag VARCHAR2(70),
+ telepules VARCHAR2(70),
+ csoportnev VARCHAR2(120)
+ );
+ 
+ INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Ausztria','Dornbirn','OJAD ifjúsági csoport');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Bulgária','Szófia','ISKRITSA Gyermek Néptánccsoport');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Csehország','Brno','Lišňáček és Bystrouška néptáncegyüttes');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Spanyolország','Alaquàs','Special K crew breaktánc csoport');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Finnország','Jokioinen','Tahdittomat ry néptánccsoport');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Man sziget','Douglas','YAC Isle of Man csoport');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('India','Új-Delhi','Mount Carmel Iskola csoportja');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Olaszország','Catania','Liceo Statale Colonna csoportja');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Japán','Aomori','Aomori kórus');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Lengyelország','Poznan','Piroska Magyar Ének és Táncegyüttes');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Lenygelország','Wołczyn','WOŁCZYNIANIE néptánccsoport');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Románia','Medgyes','Gyöngyvirág néptánccsoport');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Románia','Bögöz','Küküllő Néptánccsoport');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Románia','Nagyvárad','Szacsvay Imre Iskola tánc csoportja');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Románia','Marosszentgyörgy','Jubilate Gyermekkórus');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Románia','Sepsiszentgyörgy','Székely Mikó Kollégium néptánccsoportja');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Románia','Szentegyháza','Gyermekfilharmónia');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Törökország','Ankara','Hacettepe Egyetem Gyermek és Ifjúsági Néptánc csoportja');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Szerbia','Újvidék','Tűzrevelók Gyermek Néptáncegyüttes');
+  INSERT INTO 2018 (orszag,telepules,csoportnev)
+ VALUES ('Szerbia','Péterréve','Tisza mente Közművelődési és Népművészeti Egyesület – Csicsergők');
