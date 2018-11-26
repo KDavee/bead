80d0dae1015fe1341f55ce10af05ae4fe4a60a8e
KDavee <38099279+KDavee@users.noreply.github.com>
2018-11-26 12:14:27 +0100

    Add files via upload

--- bead/includes/config.inc.php
+++ bead/includes/config.inc.php
@@ -20,7 +20,7 @@ $lablec = array(
 $oldalak = array(
 	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Főoldal', 'menu' => 1),
 	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menu' => 1),
-	'cikkek' => array('fajl' => 'cikkek', 'szoveg' => 'Cikkek', 'menu' => 1),
+	'cikkek' => array('fajl' => 'cikkek', 'szoveg' => 'Videók', 'menu' => 1),
     'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menu' => 1),
     'regisztracio' => array('fajl' => 'urlap', 'szoveg' => 'Regisztrációáé', 'menu' => 1),
     'feldolgoz' => array('fajl' => 'feldolgoz', 'szoveg' => '', 'menu' => 0),
---
+++ bead/templates/pages/cikkek.tpl.php
@@ -0,0 +1,13 @@
+
+<h1>A fesztiválról dióhéjban</h1>
+
+<p>Főtéri nagyszínpad és meghitt zenei helyszínek a Romkertben, a belváros szívében. Többezres nemzetközi közönség, zenei workshopok és számtalan szabadidős tevékenység várja a résztvevőket Kecskeméten a kétévente megrendezésre kerülő Csiperó Fesztiválon. A nemzetközi gyermek- és ifjúsági találkozóra főként Európa, de a világ más országaiból is hívunk csoportokat, akik kecskeméti családoknál laknak a fesztivál ideje alatt. A következő évben a kecskeméti fiatalok utaznak vissza azokhoz, akiket vendégül láttak. Az ideérkező gyermek és ifjúsági csoportoktól azt kérjük, hogy hozzanak magukkal rájuk jellemző műsort, amit nyelvi akadályok nélkül tudnak bemutatni Kecskemét Főterén, szabadtéri színpadon. A rendezvényünk nem verseny, minden produkció egyenértékű. A fesztivál fő célkitűzései a kölcsönösség és az egyenlő esély mindenki számára.</p>
+<p>Azt szeretnénk, hogy a nemzetközi programból egyetlen gyermek se maradjon ki, minden programunk ingyenes.</p>
+<p>A Csiperó Fesztivál helyszíne Kecskemét, mely Magyarország közepén, a két folyó, a Duna és a Tisza között fekszik.</p>
+<p>Az alföldi város történelmi főtere remek helyszíne a fesztiváloknak.</p>
+
+<p class="center"><em>Kecskemét történetéről itt olvashatsz: <a href="http://kecskemet.hu/?r=1115">kecskemet.hu</a></em></p>
+
+
+<h1> Videóinkban látható az élet a fesztivál idején!</h1>
+<iframe class="video1" height="480"  src="https://www.youtube.com/embed/L3bn_ZXM7Lg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
